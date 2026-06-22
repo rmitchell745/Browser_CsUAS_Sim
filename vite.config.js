52438cbfa7694bd05fa11e59644c3d58ee8f6a19
Ryan Mitchell <ryan.mitchell.745@gmail.com>
2026-06-22 08:31:39 -0400

    refactor: stage wizard scenarios and add schema examples

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,27 @@
 import { resolve } from "node:path";
 import { defineConfig } from "vite";
+import htmlInclude from "vite-plugin-html-include";
 import { viteSingleFile } from "vite-plugin-singlefile";
 
+function rewriteIncludeSrc() {
+  return {
+    name: "rewrite-include-src",
+    transformIndexHtml: {
+      order: "pre",
+      handler(html) {
+        return html.replace(/<include\b([^>]*?)\ssrc=(["'])/g, "<include$1 file=$2");
+      }
+    }
+  };
+}
+
 export default defineConfig({
-  plugins: [viteSingleFile()],
+  plugins: [rewriteIncludeSrc(), htmlInclude(), viteSingleFile()],
   build: {
     assetsInlineLimit: 100000000,
     cssCodeSplit: false,
     rollupOptions: {
-      input: resolve(__dirname, "vite.html"),
+      input: resolve(__dirname, "index.html"),
       output: {
         inlineDynamicImports: true
       }
